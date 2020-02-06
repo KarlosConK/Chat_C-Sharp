b16375172184db2ed96d7f9ff4dc0e9418dd0574
Carlos Sánchez Riaza <sanchez.riaza.carlos_@hotmail.com>
2020-02-06 18:26:33 +0100

    2

--- Chat/Chat/Conexión-Sockets.cs
+++ Chat/Chat/Conexión-Sockets.cs
@@ -11,6 +11,7 @@ namespace Chat
     class Conexion_Sockets
     {
         Socket ServerSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
-
+        IPEndPoint Direccion = new IPEndPoint(IPAddress.Parse("127.0.0.1"),8000);
     }
 }
+//https://www.youtube.com/watch?v=x-mPC3KINrE
