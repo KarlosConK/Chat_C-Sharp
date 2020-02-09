5eefb9d10ce240148ce84f1b4575a6539de634d6
Carlos Sánchez Riaza <sanchez.riaza.carlos_@hotmail.com>
2020-02-09 13:55:24 +0100

    a

--- Chat/Chat/Conexión-Sockets.cs
+++ Chat/Chat/Conexión-Sockets.cs
@@ -12,6 +12,16 @@ namespace Chat
     {
         Socket ServerSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
         IPEndPoint Direccion = new IPEndPoint(IPAddress.Parse("127.0.0.1"),8000);
+        public void Conectar()
+        {
+            ServerSocket.Bind(Direccion);
+            ServerSocket.Listen(2);
+            Socket Escucha = ServerSocket.Accept();
+
+            byte[] buffer = new byte[1024];
+            int a = Escucha.Receive(buffer,0,buffer.Length,0);
+        }
+        
     }
 }
 //https://www.youtube.com/watch?v=x-mPC3KINrE
